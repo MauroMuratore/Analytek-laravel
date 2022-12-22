3a0a662ecd03e08c701d4c050ea65c037a2a46f2
Murotek <m.muratore001@studenti.unibs.it>
2022-12-22 11:55:07 +0100

    refactoring addModal

--- public/js/Analytek/send-use-case.js
+++ public/js/Analytek/send-use-case.js
@@ -9,9 +9,9 @@ $(document).ready(function() {
     }
 });
 
-function checkEndPage(){
-    //controllo che sia l'ultima pagina
+//controllo che sia l'ultima pagina
     //se è l'ultima pagina, invio i dati
+function checkEndPage(){
     let page = window.location.pathname;
     let last_page = getCookie("last_page");
     if(last_page == page){
@@ -31,7 +31,6 @@ function getUseCases() {
         url: address,
         type: "GET",
         success: function (result) {
-
             addModal(result);
         },
         error: function (error) {
@@ -42,94 +41,51 @@ function getUseCases() {
 
 //function to add modal
 function addModal(useCases){
-    //creo il modal
-    let modal = document.createElement("div");
-    modal.setAttribute("id", "myModal");
-    modal.classList.add("modal");
-    modal.setAttribute("style", "display: block; background-color: transparent;");
-    modal.setAttribute("data-bs-backdrop", "static")
-    modal.setAttribute("data-bs-keyboard","false");
-    //creo e aggiungo il modal dialog
-    let modal_dialog = document.createElement("div");
-    modal.append(modal_dialog);
-    modal_dialog.classList.add("modal-dialog");
-
-    //creo e aggiungo il modal content
-    let modal_content = document.createElement("div");
-    modal_dialog.append(modal_content);
-    modal_content.classList.add("modal-content");
-    //creo e aggiungo il modal header
-    let modal_header = document.createElement("div");
-    modal_content.append(modal_header);
-    modal_header.classList.add("modal-header");
-
-    //creo e aggiungo il modal title
-    let modal_title = document.createElement("h4");
-    modal_header.append(modal_title);
-    modal_header.setAttribute("style", "background-color: black;");
-    modal_title.classList.add("modal-title");
-    modal_title.innerHTML = "Choose a use case";
-
-    //creo e aggiungo il modal body
-    let modal_body = document.createElement("div");
-    modal_content.append(modal_body);
-    modal_body.classList.add("modal-body");
-    let row = document.createElement("div");
-    row.classList.add("row");
-    modal_body.append(row);
-    for(let i = 0; i < useCases.length; i++){
-        let col = document.createElement("div");
-        col.classList.add("col-12");
-        row.append(col);
-        let buttonUseCase = document.createElement("button");
-        buttonUseCase.setAttribute("type", "button");
-        buttonUseCase.classList.add("btn");
-        buttonUseCase.classList.add("btn-primary");
-        buttonUseCase.classList.add("my-2");
-        let id = useCases[i].id;
-        let last_page = useCases[i].last_page;
-        buttonUseCase.setAttribute("id", "useCase" + id);
-        buttonUseCase.innerHTML = useCases[i].name;
-        col.append(buttonUseCase);
-        buttonUseCase.onclick = function(){
-            //salvo il use case scelto in un cookie
-            setCookie("use_case", id);
-            setCookie("last_page", last_page);
-            //chiudo il modal
-            //$('#myModal').modal("hide");
-            $('#myModal').remove();
-
-        }
-    }
-    modal_body.setAttribute("style", "background-color: black;");
-    
-
-    //creo e aggiungo il modal footer
-    let modal_footer = document.createElement("div");
-    modal_footer.setAttribute("style", "background-color: black;");
-    modal_content.append(modal_footer);
-    modal_footer.classList.add("modal-footer");
-    let buttonClose = document.createElement("button");
-    buttonClose.setAttribute("type", "button");
-    buttonClose.classList.add("btn");
-    buttonClose.classList.add("btn-secondary");
-    
-    buttonClose.setAttribute("data-dismiss", "modal");
-    buttonClose.innerHTML = "Close";
-    buttonClose.onclick = function(){
-        //salvo il use case scelto in un cookie
-        setCookie("use_case", "no_use_case");
-        setCookie("last_page", "no_end_page");
-        //chiudo il modal
-        //$('#myModal').modal("hide");
-        $('#myModal').remove();
-    }
-    modal_footer.append(buttonClose);
-    
-
-    $('body').append(modal);
+    let modal_html =    "<div class='modal' id='myModal' style='background-color: transparent;' data-bs-backdrop='static' data-bs-keyboard='false' tabindex='-1' aria-labelledby='staticBackdropLabel' > \n \
+                            <div class='modal-dialog'> \n \
+                                <div class='modal-content'> \n \
+                                    <div class='modal-header' style='background-color: black;'> \n \
+                                        <h4 class='modal-title'>Choose a use case</h4> \n \
+                                    </div> \n \
+                                    <div class='modal-body' style='background-color: black;'> \n \
+                                        <div class='row'> \n \
+                                        </div> \n \
+                                    </div> \n \
+                                    <div class='modal-footer' style='background-color: black;'> \n \
+                                        <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button> \n \
+                                    </div> \n \
+                                </div> \n \
+                            </div> \n \
+                        </div> \n";
+    $('body').append(modal_html);
+    createUseCaseButtons(useCases);
+    $('#myModal').modal("show");
 }
 
+//function create for use case buttons in modal
+function createUseCaseButtons(use_cases){
+    use_cases.forEach(element => {
+        let col = document.createElement("div");
+        col.classList.add("col-12");
+        let button = document.createElement("button");
+        col.append(button);
+        button.setAttribute("type", "button");
+        button.classList.add("btn");
+        button.classList.add("btn-primary");
+        button.classList.add("my-2");
+        button.setAttribute("id", "useCase" + element.id);
+        button.innerHTML = element.name;
+        button.onclick = function(){
+            //salvo il use case scelto in un cookie
+            setCookie("use_case", element.id);
+            setCookie("last_page", element.last_page);
+            //chiudo il modal
+            $('#myModal').modal("hide");
+            //$('#myModal').remove();
+        }
+        $('.modal-body > .row').append(col);
+    });
+}
 //function to send use case
 function postPerfomance(){
     let address = "/api/use_cases/";
